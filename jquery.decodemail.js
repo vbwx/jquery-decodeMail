1f8dced152c825f9513b41c486e0fea794547537
Bernhard Waldbrunner <vbwx01@gmail.com>
2016-10-11 02:08:01 +0200

    Initial commit

---
+++ jquery.decodemail.js
@@ -0,0 +1,26 @@
+// jQuery decodeMail plugin
+// Copyright (C) 2016 Bernhard Waldbrunner
+/*
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// <a href="mail:76627778303140676D61696C2E636F6D?subject=...">My e-mail address is 76627778303140676D61696C2E636F6D!</a>
+
+jQuery(function($) {
+	$('a[href^="mail:"]').each(function() {
+		var $a = $(this), hex = $a.attr('href').replace(/^mail:|[^\w:].*$/g, '');
+		var mail = $('<div/>').html(hex.replace(/[A-Z0-9]{2}/g, '&#x$&;')).text();
+		$a.attr('href', 'mailto:' + mail).html($a.html().replace(RegExp("\\b" + hex + "\\b"), mail));
+	});
+});
